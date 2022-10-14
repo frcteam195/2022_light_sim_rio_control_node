c857d1638e6de1017c7e8e5448278640761438a0
Michael Todd <eltodd@gmail.com>
2022-10-13 20:44:39 -0400

    Cont updates

--- src/light_sim_2022_rio_control_node.cpp
+++ src/light_sim_2022_rio_control_node.cpp
@@ -18,6 +18,15 @@
 
 #define RATE (100)
 
+#define STR_PARAM(s) #s
+#define CKSP(s) ckgp( STR_PARAM(s) )
+std::string ckgp(std::string instr)
+{
+	std::string retVal = ros::this_node::getName();
+	retVal += "/" + instr;
+	return retVal;
+}
+
 ros::NodeHandle* node;
 std::atomic<bool> sigintCalled;
 void sigint_handler(int sig)
@@ -26,6 +35,10 @@ void sigint_handler(int sig)
 	sigintCalled = true;
 	ros::shutdown();
 }
+
+static std::vector<float> gear_ratio_to_output_shaft;
+static std::vector<float> motor_ticks_per_revolution;
+static std::vector<float> motor_ticks_velocity_sample_window;
 
 // void gazebo_link_states_callback(const gazebo_msgs::LinkStates &msg)
 // {
@@ -74,15 +87,55 @@ void publish_motor_status()
     status_publisher.publish(motor_status);
 }
 
+void load_config_params()
+{
+    bool received_data = false;
+    received_data = node->getParam(CKSP(gear_ratio_to_output_shaft), gear_ratio_to_output_shaft);
+    if(!received_data)
+    {
+        ROS_ERROR("COULD NOT LOAD GEAR RATIOS, using 1.0");
+        for(uint32_t i = 0;
+            i < 20;
+            i++)
+        {
+            gear_ratio_to_output_shaft.push_back(1.0);
+        }
+    }
+
+    received_data = node->getParam(CKSP(motor_ticks_per_revolution), motor_ticks_per_revolution);
+    if(!received_data)
+    {
+        ROS_ERROR("COULD NOT LOAD ENCODER TICK COUNT, using 2048");
+        for(uint32_t i = 0;
+            i < 20;
+            i++)
+        {
+            motor_ticks_per_revolution.push_back(2048.0);
+        }
+    }
+
+    received_data = node->getParam(CKSP(motor_ticks_velocity_sample_window), motor_ticks_velocity_sample_window);
+    if(!received_data)
+    {
+        ROS_ERROR("COULD NOT LOAD ENCODER SAMPLE WINDOW, using 0.1");
+        for(uint32_t i = 0;
+            i < 20;
+            i++)
+        {
+            motor_ticks_velocity_sample_window.push_back(0.1);
+        }
+    }
+}
+
 void motor_control_callback(const rio_control_node::Motor_Control &msg)
 {
     // double right_side_rpm;
     // double left_side_rpm;
 
     // int left_master_id, right_master_id;
-    
+
     // double drive_kV;
-    
+
     // bool data_verified = true;
 
     // data_verified &= node->getParam("/light_sim_rio_control_node/left_master_id", left_master_id);
@@ -104,7 +157,7 @@ void motor_control_callback(const rio_control_node::Motor_Control &msg)
             rio_control_node::Motor_Info motor_info;
             motor_info.bus_voltage = 12;
             motor_info.id = (*i).id;
-            motor_info.sensor_velocity = (*i).output_value * 4000;
+            motor_info.sensor_velocity = (*i).output_value * 6380.0 / gear_ratio_to_output_shaft[(*i).id];
             motor_info_map[motor_info.id] = motor_info;
         }
         else if ((*i).control_mode == rio_control_node::Motor::MOTION_MAGIC)
@@ -120,7 +173,7 @@ void motor_control_callback(const rio_control_node::Motor_Control &msg)
             motor_info.sensor_position = ((*i).output_value + (last_position * 5.0)) / 6.0;
             motor_info_map[motor_info.id] = motor_info;
         }
-        
+
         // if ((*i).id == left_master_id)
         // {
         //     if((*i).control_mode == rio_control_node::Motor::PERCENT_OUTPUT)
@@ -161,7 +214,7 @@ void publish_robot_status()
 void linux_joystick_subscriber(const sensor_msgs::Joy &msg)
 {
     static ros::Publisher joystick_publisher = node->advertise<rio_control_node::Joystick_Status>("/JoystickStatus", 100);
-    
+
     rio_control_node::Joystick_Status joystick_status;
     rio_control_node::Joystick joystick;
 
@@ -193,6 +246,8 @@ int main(int argc, char **argv)
 	ros::NodeHandle n;
 	node = &n;
     ros::Rate rate(RATE);
+
+    load_config_params();
 
 	ros::Subscriber motorConfig = node->subscribe("/MotorConfiguration", 100, motor_config_callback);
 	ros::Subscriber motorControl = node->subscribe("/MotorControl", 100, motor_control_callback);
